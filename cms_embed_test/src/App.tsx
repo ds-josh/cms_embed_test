910ef5e352e88f6cc6a78d15f1d7d8c30de5e207
jameswesc <jms.wsc@gmail.com>
2025-07-30 17:19:03 +1000

    local version to better mimic production

--- cms_embed_test/src/App.tsx
+++ cms_embed_test/src/App.tsx
@@ -4,25 +4,25 @@ import { Cube } from "./components/Cube";
 import { useRef } from "react";
 
 function App() {
-	const scrollRef = useRef(null!);
+    const scrollRef = useRef(null!);
 
-	const { scrollYProgress } = useScroll({
-		target: scrollRef,
-		offset: ["start start", "end end"],
-	});
+    const { scrollYProgress } = useScroll({
+        target: scrollRef,
+        offset: ["start start", "end end"],
+    });
 
-	return (
-		<div ref={scrollRef} className="w-full h-[300vh] top-0">
-			<motion.div className="w-full h-[100vh] border-1 border-pink-300 sticky top-0 flex">
-				<div className="flex-1 h-screen flex items-center justify-center">
-					<Prose scrollProgress={scrollYProgress} />
-				</div>
-				<div className="flex-1 h-screen">
-					<Cube scrollProgress={scrollYProgress} />
-				</div>
-			</motion.div>
-		</div>
-	);
+    return (
+        <div ref={scrollRef} className="w-full h-[300vh] top-0">
+            <motion.div className="w-full h-[100vh] border-1 border-pink-300 sticky top-0 flex">
+                <div className="flex-1 h-screen flex items-center justify-center">
+                    <Prose scrollProgress={scrollYProgress} />
+                </div>
+                <div className="flex-1 h-screen">
+                    <Cube scrollProgress={scrollYProgress} />
+                </div>
+            </motion.div>
+        </div>
+    );
 }
 
 export default App;
