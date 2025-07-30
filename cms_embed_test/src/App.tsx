1da28aa80a30b06677f6886eada935be717017a4
joshtedbird <josh.ted.bird@gmail.com>
2025-07-30 15:44:31 +1000

    sticky test added

--- cms_embed_test/src/App.tsx
+++ cms_embed_test/src/App.tsx
@@ -1,18 +1,26 @@
-import { useScroll } from "motion/react";
+import { motion, useScroll } from "motion/react";
 import { Prose } from "./components/Prose";
 import { Cube } from "./components/Cube";
+import { useRef } from "react";
 
 function App() {
-	const { scrollYProgress } = useScroll();
+	const scrollRef = useRef(null!);
+
+	const { scrollYProgress } = useScroll({
+		target: scrollRef,
+		offset: ["start start", "end end"],
+	});
 
 	return (
-		<div className="w-full h-[300vh] bg-white">
-			<div className="w-1/2 h-screen fixed flex items-center justify-center">
-				<Prose scrollProgress={scrollYProgress} />
-			</div>
-			<div className="w-1/2 h-screen fixed left-1/2">
-				<Cube scrollProgress={scrollYProgress} />
-			</div>
+		<div ref={scrollRef} className="w-full h-[300vh] top-0">
+			<motion.div className="w-full h-[100vh] border-1 border-pink-300 sticky top-0 flex">
+				<div className="flex-1 h-screen flex items-center justify-center">
+					<Prose scrollProgress={scrollYProgress} />
+				</div>
+				<div className="flex-1 h-screen">
+					<Cube scrollProgress={scrollYProgress} />
+				</div>
+			</motion.div>
 		</div>
 	);
 }
